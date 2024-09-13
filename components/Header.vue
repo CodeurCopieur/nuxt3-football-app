38e568f556c0213e8c820f72c46dbbcbc7b44d74
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-09-13 10:20:00 +0200

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,3 +1,20 @@
 <template>
-  <h1>HEADER</h1>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/" aria-label="Accueil">Accueil</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/leagues" aria-label="Leagues">Leagues</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/players" aria-label="Players">Players</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/teams" aria-label="Teams">Teams</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
