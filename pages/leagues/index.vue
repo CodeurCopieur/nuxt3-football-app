f491084d263381ddadc844ecde1dabe60116085b
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2024-09-13 10:19:41 +0200

    Create index.vue

---
+++ pages/leagues/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  interface State {
+    league: string;
+  }
+  const state = reactive<State>({
+    leagues: "leagues",
+  });
+</script>
+
+<template>
+ <h1>Home Leagues</h1>
+ <Content :type="state.leagues" />
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
